8c6e0ac66b4be4a6d11a66c6f5ac7810931cb964
db1238b <db1238b@gre.ac.uk>
2019-02-09 12:49:20 +0000

    Further Registration changes

--- RegisterController.php
+++ RegisterController.php
@@ -1,27 +1,36 @@
 <?php
 require "DataActionService.php";
-require "DataCollectionService.php";
 
 header('Content-Type: application/json');
-extract($_POST);
+
 $errorCode = null;
 $errorMessage = null;
 
+$email = $_POST['email'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$password = $_POST['password'];
+$username = $_POST['username'];
+$department = $_POST['department'];
 
-$departmentID = GetDepartmentID($department)
-if($departmentID < 1 ){
-    $result = registerUser($email, $first_name, $last_name, $departmentID, $password, $username);
-    if($result == 1062){
-        //https response code actually needs to be set see how i have done it in the login controller
-        $return = array("errorCode"=>400,
-                       "errorMessage"=>"Duplicate User");
-    }else{
-        $return = array("register"=>true);
-    }
+
+unset($_POST['email']);
+unset($_POST['first_name']);
+unset($_POST['last_name']);
+unset($_POST['department']);
+unset($_POST['password']);
+unset($_POST['username']);
+
+$departmentID = 3;
+$result = registerUser($email, $first_name, $last_name, $departmentID, $password, $username);
+if($result === 1062){
+    //https response code actually needs to be set see how i have done it in the login controller
+    $return = array("errorCode" => 400,
+                   "errorMessage" => "Duplicate User");
 }else{
-    $return = array("errorCode"=>400,
-                   "errorMessage"=>"incorect Department");  
+    $return = array("register"=>true);
 }
+
 echo json_encode($return);
 die();
 
